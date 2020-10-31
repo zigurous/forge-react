7b835021a21d430f8d830b8b2ebe4b0ccc016f15
Adam Graham <adam.zigurous@gmail.com>
2020-10-31 03:09:34 -0500

    Convert svg images to inline svg

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -5,8 +5,6 @@ import Logo, { logoThemes } from './Logo';
 import Modal from './Modal';
 import SocialNavLinks from './SocialNavLinks';
 import socialLinks from '../socials';
-import closeIcon from '../images/icons/close-white.svg';
-import menuIcon from '../images/icons/menu-white.svg';
 import '../styles/nav-menu.css';
 
 const NavMenu = ({
@@ -65,7 +63,11 @@ const NavMenu = ({
             >
               <img
                 alt={isOpen ? 'Close Menu' : 'Open Menu'}
-                src={isOpen ? closeIcon : menuIcon}
+                src={
+                  isOpen
+                    ? "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='white' width='36px' height='36px' %3e%3cpath d='M0 0h24v24H0z' fill='none' /%3e%3cpath d='M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z' /%3e%3c/svg%3e"
+                    : "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='white' width='36px' height='36px' %3e%3cpath d='M0 0h24v24H0z' fill='none' /%3e%3cpath d='M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z' /%3e%3c/svg%3e"
+                }
               />
             </button>
           </div>
