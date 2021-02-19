bbc1c1cfcc2098619f273ad820eaa26404f08811
Adam Graham <adam@zigurous.com>
2021-02-19 02:55:44 -0600

    Fix modal scrollbar reflow

--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -1,21 +1,20 @@
 import { useEffect, useState } from 'react';
-import { getScrollbarWidth, hasVerticalScroll } from '../utils/scrolling';
+import { getScrollbarWidth } from '../utils/scrolling';
 
 const useModal = (open = false, reflow = false) => {
   const [isOpen, setIsOpen] = useState(Boolean(open));
 
   useEffect(() => {
     if (isOpen) {
-      document.body.classList.add('overflow-hidden');
-
       if (reflow) {
-        const canScroll = hasVerticalScroll(document.body);
         const scrollbarWidth = getScrollbarWidth();
 
-        if (canScroll && scrollbarWidth > 0) {
+        if (scrollbarWidth > 0) {
           document.body.style.paddingRight = `${scrollbarWidth}px`;
         }
       }
+
+      document.body.classList.add('overflow-hidden');
     } else {
       document.body.classList.remove('overflow-hidden');
 
