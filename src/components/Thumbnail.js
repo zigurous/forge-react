15bb820c2e1af52a561d387b1bf8382e3ee156c1
Adam Graham <adam@zigurous.com>
2021-03-31 06:58:29 -0500

    Change thumbnail animations and hover state

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -1,60 +1,86 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import ProgressiveImage from './ProgressiveImage';
 import '../styles/thumbnail.css';
 
 const Thumbnail = ({
+  animated = false,
+  animation = 'fade-in-up',
   children,
   className,
   ElementType = 'a',
   height = 'auto',
   image,
   imageProps = {},
+  index,
   placeholder,
   placeholderProps = {},
   rounded = true,
   shadow = true,
   style,
   ...props
-}) => (
-  <ElementType
-    {...props}
-    className={classNames(
-      'thumbnail',
-      { 'thumbnail--rounded': rounded },
-      { 'shadow-sm': shadow },
-      className
-    )}
-  >
-    {image && (
-      <ProgressiveImage
-        alt={imageProps.alt}
-        imageProps={{
-          ...imageProps,
-          className: classNames('img-fluid', imageProps.className),
-          height: imageProps.height || height,
-        }}
-        placeholder={placeholder}
-        placeholderProps={{
-          ...placeholderProps,
-          className: classNames('img-fluid', placeholderProps.className),
-          height: placeholderProps.height || height,
-        }}
-        src={image}
-      />
-    )}
-    {children}
-  </ElementType>
-);
+}) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    if (animated) {
+      setTimeout(() => {
+        if (ref && ref.current) {
+          ref.current.style.animation = 0;
+        }
+      }, 600);
+    }
+  }, [animated, ref]);
+
+  return (
+    <ElementType
+      {...props}
+      className={classNames(
+        'thumbnail',
+        { 'thumbnail--rounded': rounded },
+        { 'thumbnail--shadow': shadow },
+        {
+          'animation-short': animated,
+          [`animation-delay-${index + 1}`]: animated && index >= 0,
+          [animation]: animated,
+        },
+        className
+      )}
+      ref={ref}
+    >
+      {image && (
+        <ProgressiveImage
+          alt={imageProps.alt}
+          imageProps={{
+            ...imageProps,
+            className: classNames('img-fluid', imageProps.className),
+            height: imageProps.height || height,
+          }}
+          placeholder={placeholder}
+          placeholderProps={{
+            ...placeholderProps,
+            className: classNames('img-fluid', placeholderProps.className),
+            height: placeholderProps.height || height,
+          }}
+          src={image}
+        />
+      )}
+      {children}
+    </ElementType>
+  );
+};
 
 Thumbnail.propTypes = {
+  animated: PropTypes.bool,
+  animation: PropTypes.string,
   children: PropTypes.node,
   className: PropTypes.string,
   ElementType: PropTypes.elementType,
   height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   image: PropTypes.string,
   imageProps: PropTypes.object,
+  index: PropTypes.number,
   placeholder: PropTypes.string,
   placeholderProps: PropTypes.object,
   rounded: PropTypes.bool,
