ede6e903d8fb810768a1c589853f71f632c2b77f
Adam Graham <adam@zigurous.com>
2021-03-31 13:20:33 -0500

    Add usePagination hook

---
+++ src/hooks/usePagination.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+const usePagination = (items, itemsPerPage = 6, storageKey = undefined) => {
+  const [page, setPage] = useState(0);
+  const startIndex = page * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const totalPages = Math.ceil(items.length / itemsPerPage);
+
+  useEffect(() => {
+    if (storageKey && sessionStorage) {
+      const storedValue = sessionStorage.getItem(storageKey);
+      const storedPageNumber = parseInt(storedValue, 10);
+
+      if (!Number.isNaN(storedPageNumber)) {
+        setPage(storedPageNumber);
+      }
+
+      sessionStorage.removeItem(storageKey);
+    }
+  }, [storageKey, items]);
+
+  const state = {
+    page,
+    paginated: totalPages > 1,
+    currentPage: page,
+    totalPages,
+    itemsPerPage,
+    items: items.slice(startIndex, endIndex),
+  };
+
+  return [state, setPage];
+};
+
+export default usePagination;
