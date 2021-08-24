4e9d6f8b33296510f027114a618fba9625fcb1db
Adam Graham <adam@zigurous.com>
2021-08-24 02:32:30 -0500

    Replace navmenu custom svg with material icon

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
+import Icon from './Icon';
 import Link from './Link';
 import ReactPortal from './ReactPortal';
 import SocialNavLinks from './SocialNavLinks';
@@ -41,6 +42,7 @@ const NavMenu = ({
   return (
     <React.Fragment>
       <button
+        aria-label={isOpen ? 'Close' : 'Open'}
         className={classNames(
           'navmenu__button',
           { 'z-index-modal': isOpen },
@@ -49,14 +51,7 @@ const NavMenu = ({
         onClick={() => setIsOpen(!isOpen)}
         size="small"
       >
-        <img
-          alt={isOpen ? 'Close Menu' : 'Open Menu'}
-          src={
-            isOpen
-              ? "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='white' width='36px' height='36px' %3e%3cpath d='M0 0h24v24H0z' fill='none' /%3e%3cpath d='M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z' /%3e%3c/svg%3e"
-              : "data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='white' width='36px' height='36px' %3e%3cpath d='M0 0h24v24H0z' fill='none' /%3e%3cpath d='M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z' /%3e%3c/svg%3e"
-          }
-        />
+        <Icon name={isOpen ? 'close' : 'menu'} material />
       </button>
       {isOpen && (
         <ReactPortal rootElement={portalRootElement}>
