f28465bde672cc3cb26008ced6ae3186a0708af8
Adam Graham <adam@zigurous.com>
2024-10-28 15:37:05 -0500

    Add align property to <ButtonGroup>

--- src/components/ButtonGroup.tsx
+++ src/components/ButtonGroup.tsx
@@ -2,10 +2,12 @@ import classNames from 'classnames';
 import React from 'react';
 
 export type ButtonGroupProps = {
+  align?: 'center' | 'start' | 'end' | 'stretch';
   layout?: 'horizontal' | 'vertical';
 } & React.ComponentPropsWithRef<'div'>;
 
 export default function ButtonGroup({
+  align = 'center',
   children,
   className,
   layout = 'horizontal',
@@ -16,6 +18,7 @@ export default function ButtonGroup({
       className={classNames(
         'btn-group',
         { [`btn-group--${layout}`]: layout },
+        { [`align-${align}`]: align },
         className,
       )}
       {...rest}
