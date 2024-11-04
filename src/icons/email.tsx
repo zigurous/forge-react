3d84be1eed430919e60c98f492a3ba1ad8edb9d0
Adam Graham <adam@zigurous.com>
2024-11-04 02:59:59 -0600

    Reduce scale of email icon from 1.2 to 1.1

--- src/icons/email.tsx
+++ src/icons/email.tsx
@@ -9,7 +9,7 @@ export default function EmailIcon({ ...rest }: React.SVGProps<SVGSVGElement>) {
       viewBox="0 0 184 184"
       width="24px"
       height="24px"
-      transform="scale(1.2)"
+      transform="scale(1.1)"
       {...rest}
     >
       <title>Email</title>
