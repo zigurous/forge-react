6098b9cf5ca0ff4f5e4afa618b26285cc0c4379e
Adam Graham <adam@zigurous.com>
2024-11-16 17:51:51 -0600

    Fix search debouncing by implementing useCallback

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -19,13 +19,28 @@ export function useSearch<T>(
   const [query, setQuery] = useState('');
   const [results, setResults] = useState<T[] | null>(null);
 
-  const search: (input: string) => T[] = useCallback(
-    (input: string) => {
-      const query = input.toLowerCase().replace(' ', '').trim();
-      return items.filter(item => matches(item, query));
+  const search = useCallback(
+    (input: string | null | undefined) => {
+      if (input) {
+        const query = input.toLowerCase().replace(' ', '').trim();
+        return items.filter(item => matches(item, query));
+      } else {
+        return null;
+      }
     },
     [items, matches],
   );
+
+  const onSearch = useCallback(
+    (input: string | null | undefined) => {
+      setResults(search(input));
+    },
+    [search],
+  );
+
+  const onChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  }, []);
 
   useEffect(() => {
     if (
@@ -45,16 +60,8 @@ export function useSearch<T>(
   const state: SearchState<T> = {
     query,
     results,
-    onChange: event => {
-      setQuery(event.target.value);
-    },
-    onSearch: input => {
-      if (input) {
-        setResults(search(input));
-      } else {
-        setResults(null);
-      }
-    },
+    onSearch,
+    onChange,
   };
 
   return [state, setQuery, setResults];
