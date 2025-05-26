6c01cd8d5f9dd0e0473780bbbde628286b0ec334
Adam Graham <adam@zigurous.com>
2025-05-26 06:50:08 -0500

    Add capitalizeFirstLetter formatting function

--- src/utils/formatting.ts
+++ src/utils/formatting.ts
@@ -1,3 +1,7 @@
+export function capitalizeFirstLetter(str: string): string {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 export function toTitleCase(title: string): string {
   return title.replace(/(^\w|\s\w)/g, l => l.toUpperCase());
 }
