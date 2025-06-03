1367f67ba43845f699ff04cba1ca7ac13f9f6f27
Adam Graham <adam@zigurous.com>
2025-06-03 06:34:14 -0500

    Add optional label to <Input> component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,6 +3,8 @@
 import classNames from 'classnames';
 import React, { useRef, useState } from 'react';
 import Icon, { type IconProps } from './Icon';
+import Stack from './Stack';
+import Text from './Text';
 import type { IconElement } from '../types';
 
 export type InputProps = {
@@ -11,6 +13,7 @@ export type InputProps = {
   icon?: IconElement;
   iconAlignment?: 'leading' | 'trailing';
   iconProps?: IconProps<'i'>;
+  label?: string;
   onBlur?: React.FocusEventHandler<HTMLInputElement>;
   onFocus?: React.FocusEventHandler<HTMLInputElement>;
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
@@ -22,6 +25,7 @@ export default function Input({
   icon,
   iconAlignment = 'trailing',
   iconProps,
+  label,
   onBlur,
   onFocus,
   size,
@@ -30,39 +34,59 @@ export default function Input({
   const ref = useRef<HTMLInputElement>(null);
   const [focused, setFocused] = useState(false);
   return (
-    <div
-      className={classNames(
-        'input-wrapper',
-        {
-          [`input-wrapper--${size}`]: size,
-          [`input-wrapper--icon-${iconAlignment}`]: icon && iconAlignment,
-          focused: focused,
-          disabled: disabled,
-        },
-        className,
-      )}
-    >
-      <input
-        className={classNames({
-          empty: !Boolean(ref.current?.value || rest.value),
-        })}
-        disabled={disabled}
-        onBlur={event => {
-          setFocused(false);
-          if (onBlur) {
-            onBlur(event);
-          }
-        }}
-        onFocus={event => {
-          setFocused(true);
-          if (onFocus) {
-            onFocus(event);
-          }
-        }}
-        ref={ref}
-        {...rest}
-      />
-      {icon && <Icon aria-hidden icon={icon} {...iconProps} />}
-    </div>
+    <InputContainer label={label}>
+      <div
+        className={classNames(
+          'input-wrapper',
+          {
+            [`input-wrapper--${size}`]: size,
+            [`input-wrapper--icon-${iconAlignment}`]: icon && iconAlignment,
+            focused: focused,
+            disabled: disabled,
+          },
+          className,
+        )}
+      >
+        <input
+          className={classNames({
+            empty: !Boolean(ref.current?.value || rest.value),
+          })}
+          disabled={disabled}
+          onBlur={event => {
+            setFocused(false);
+            if (onBlur) {
+              onBlur(event);
+            }
+          }}
+          onFocus={event => {
+            setFocused(true);
+            if (onFocus) {
+              onFocus(event);
+            }
+          }}
+          ref={ref}
+          {...rest}
+        />
+        {icon && <Icon aria-hidden icon={icon} {...iconProps} />}
+      </div>
+    </InputContainer>
+  );
+}
+
+function InputContainer({
+  children,
+  label,
+}: {
+  children: React.ReactNode;
+  label?: string;
+}) {
+  if (!label) return <>{children}</>;
+  return (
+    <Stack className="input-container" layout="vertical" inline>
+      <Text color="subtle" className="ml-xxxs mb-xxs" type="caption">
+        {label}
+      </Text>
+      {children}
+    </Stack>
   );
 }
